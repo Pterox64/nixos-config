7a917da4a1b91503c9a4beb98551cad16bd777bd
Egor Platohin <e.platohin@eteron.ru>
2024-10-10 00:20:51 +0300

    Mutable user

--- nixos/modules/users.nix
+++ nixos/modules/users.nix
@@ -1,13 +1,12 @@
 { pkgs, lib, ... }:
 {
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.mutableUsers = false;
+  users.mutableUsers = true;
   users.defaultUserShell = pkgs.zsh;
   users.users.egor = {
     isNormalUser = true;
     home = "/home/egor";
     description = "Egor Platokhin";
-    hashedPassword = lib.strings.fileContents ../user.egor.secret;
     extraGroups = [
       "wheel"
       "networkmanager"
