2b1e99f4e0f5556b999e0e4e76de6fd858d8b198
Egor Platohin <e.platohin@eteron.ru>
2024-11-04 22:24:43 +0300

    Refact

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -8,57 +8,66 @@
 
   disabledModules = [ ./modules/fprintd.nix ];
 
-  hardware.enableRedistributableFirmware = true;
-  hardware.cpu.amd.ryzen-smu.enable = true;
-  hardware.sensor.hddtemp.enable = true;
-  hardware.sensor.hddtemp.drives = [ "/dev/nvme0n1" ];
+  hardware = {
+    enableRedistributableFirmware = true;
+    cpu.amd.ryzen-smu.enable = true;
+    sensor.hddtemp = {
+      enable = true;
+      drives = [ "/dev/nvme0n1" ];
+    };
+  };
+  powerManagement = {
+    enable = true;
+    powertop.enable = true;
+  };
 
-  powerManagement.enable = true;
-  powerManagement.powertop.enable = true;
+  nix = {
+    settings = {
+      allowed-users = [ "@wheel" ];
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
 
-  system.autoUpgrade.enable = true;
+  services = {
+    fwupd.enable = true;
 
-  nix.settings.allowed-users = [ "@wheel" ];
+    # # Enable the X11 windowing system.
+    xserver.enable = true;
 
-  services.fwupd.enable = true;
+    # Configure keymap in X11
+    # xserver.xkb = {
+    #   layout = "us,ru";
+    #   options = "grp:win_space_toggle";
+    # };
 
-  # # Enable the X11 windowing system.
-  services.xserver.enable = true;
+    # Enable CUPS to print documents.
+    # printing.enable = true;
 
-  # Configure keymap in X11
-  # services.xserver.xkb = {
-  #   layout = "us,ru";
-  #   options = "grp:win_space_toggle";
-  # };
+    yubikey-agent.enable = true;
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+    # Enable touchpad support (enabled default in most desktopManager).
+    libinput.enable = true;
 
-  services.yubikey-agent.enable = true;
+    # List services that you want to enable:
+
+    # Enable the OpenSSH daemon.
+    # openssh.enable = true;
+  };
+
   # xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   # xdg.portal.config.common.default = "gtk";
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.libinput.enable = true;
-
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 30d";
-  };
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -71,22 +80,25 @@
   # accidentally delete configuration.nix.
   # system.copySystemConfiguration = true;
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system = {
+    # This option defines the first version of NixOS you have installed on this particular machine,
+    # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+    #
+    # Most users should NEVER change this value after the initial install, for any reason,
+    # even if you've upgraded your system to a new NixOS release.
+    #
+    # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+    # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+    # to actually do that.
+    #
+    # This value being lower than the current NixOS release does NOT mean your system is
+    # out of date, out of support, or vulnerable.
+    #
+    # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+    # and migrated your data accordingly.
+    #
+    # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+    stateVersion = "24.05"; # Did you read the comment?
+    autoUpgrade.enable = true;
+  };
 }
