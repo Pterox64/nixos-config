dbff5c37b585369cce62252e9b1abb8fa4d60b65
Egor Platohin <e.platohin@eteron.ru>
2025-01-31 00:22:51 +0300

    Add restore ALSA settings

--- machines/modules/audio.nix
+++ machines/modules/audio.nix
@@ -1,13 +1,32 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    alsa-utils pulsemixer
-    wireplumber
-  ];
-  hardware.firmware = [ pkgs.sof-firmware ];
-  hardware.pulseaudio.enable = false;
+  hardware = {
+    firmware = [ pkgs.sof-firmware ];
+    pulseaudio.enable = false;
+  };
+
   services.pipewire = {
     enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    alsa-utils
+    pulsemixer
+    wireplumber
+  ];
+
+  # Сервис для восстановления звукового профиля
+  systemd.services.alsactl-restore = {
+    description = "Restore ALSA settings";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "sound.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.alsa-utils}/bin/alsactl restore";
+      RemainAfterExit = true;
+    };
   };
 }
