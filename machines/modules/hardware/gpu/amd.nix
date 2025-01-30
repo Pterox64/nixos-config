d89609e802fd27114e211def19a4a6d0aea4aeb1
Egor Platohin <e.platohin@eteron.ru>
2025-01-30 11:33:38 +0300

    Fix amdgpu

--- machines/modules/hardware/gpu/amd.nix
+++ machines/modules/hardware/gpu/amd.nix
@@ -1,5 +1,6 @@
 { pkgs, ... }:
 {
+  services.xserver.videoDrivers = [ "amdgpu" ];
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
@@ -9,4 +10,13 @@
     extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
   };
   hardware.amdgpu.amdvlk.enable = true;
+  # Установка необходимых пакетов
+  environment.systemPackages = with pkgs; [
+    vulkan-tools # Для проверки Vulkan
+    mesa # Библиотеки OpenGL
+  ];
+  environment.variables = {
+    AMD_VULKAN_ICD = "RADV"; # Использование RADV вместо AMDVLK
+    RADV_PERFTEST = "gpl"; # Включение расширенных функций Vulkan
+  };
 }
