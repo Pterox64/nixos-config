f68552dd4d80e77cb5fcc7af763d3311e66e5160
Egor Platohin <e.platohin@eteron.ru>
2025-01-31 00:23:16 +0300

    Small refactoring

--- machines/modules/hardware/gpu/amd.nix
+++ machines/modules/hardware/gpu/amd.nix
@@ -1,27 +1,34 @@
 { pkgs, ... }:
 {
-  services.xserver.videoDrivers = [ "amdgpu" ];
-  hardware.graphics = {
-    enable = true;
-    extraPackages = with pkgs; [
-      rocmPackages.clr.icd
-      amdvlk
-    ];
-    extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
-  };
-  hardware.amdgpu.amdvlk.enable = true;
-  # Установка необходимых пакетов
-  environment.systemPackages = with pkgs; [
-    vulkan-tools # Для проверки Vulkan
-    glxinfo
-    mesa # Библиотеки OpenGL
-  ];
   # Дополнительные настройки для улучшения производительности
   boot.kernelParams = [
     "amdgpu.sg_display=0" # Оптимизация для некоторых моделей AMD
+    # "amdgpu.secure_display=0" # Отключаем Secure Display
   ];
-  environment.variables = {
-    AMD_VULKAN_ICD = "RADV"; # Использование RADV вместо AMDVLK
-    RADV_PERFTEST = "gpl"; # Включение расширенных функций Vulkan
+
+  services.xserver.videoDrivers = [ "amdgpu" ];
+
+  hardware = {
+    graphics = {
+      enable = true;
+      extraPackages = with pkgs; [
+        rocmPackages.clr.icd
+        amdvlk
+      ];
+      extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
+    };
+    amdgpu.amdvlk.enable = true;
+  };
+  environment = {
+    # Установка необходимых пакетов
+    systemPackages = with pkgs; [
+      vulkan-tools # Для проверки Vulkan
+      glxinfo
+      mesa # Библиотеки OpenGL
+    ];
+    variables = {
+      AMD_VULKAN_ICD = "RADV"; # Использование RADV вместо AMDVLK
+      RADV_PERFTEST = "gpl"; # Включение расширенных функций Vulkan
+    };
   };
 }
