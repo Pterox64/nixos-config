2564ff73ab22ca8d10fefff066ab16db23f65547
Egor Platohin <e.platohin@eteron.ru>
2025-01-31 11:24:30 +0300

    Add heif support

--- home/modules/gnome/default.nix
+++ home/modules/gnome/default.nix
@@ -5,5 +5,10 @@
   home.packages = with pkgs; [
     gnome-tweaks
     gnome-terminal
+    libheif
+    gst_all_1.gst-plugins-bad
   ];
+  home.sessionVariables = {
+    GST_PLUGIN_SYSTEM_PATH_1_0 = "${pkgs.gst_all_1.gst-plugins-bad}/lib/gstreamer-1.0";
+  };
 }
