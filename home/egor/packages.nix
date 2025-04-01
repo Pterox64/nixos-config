8ca23b4b80291e65aed05304d124f07612ad5f24
Egor Platohin <e.platohin@eteron.ru>
2025-04-01 22:16:06 +0300

    Delete FreeCAD

--- home/egor/packages.nix
+++ home/egor/packages.nix
@@ -1,10 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   home.packages = with pkgs; [
     bitwarden-desktop
     cryptomator
     dbeaver-bin
-    freecad-wayland
     nekoray
     obsidian
     keepass
