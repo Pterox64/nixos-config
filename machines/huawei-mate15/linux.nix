6f89b390ac3190888973a4c37b37de1fa5a0fd5e
Egor Platohin <e.platohin@eteron.ru>
2025-04-22 18:13:51 +0300

    Remove custom kernel

--- machines/huawei-mate15/linux.nix
+++ machines/huawei-mate15/linux.nix
@@ -6,22 +6,4 @@
     "fuse"
   ];
   boot.blacklistedKernelModules = [ "aufs" ];
-  # boot.kernelPackages =
-  #   let
-  #     linux_huawei = pkgs.callPackage ../kernel/linux-6.1.3-huawei.nix { };
-  #   in
-  #   pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_huawei);
-
-  # boot.kernelModules = [
-  #   "snd_soc_es8316"
-  #   "snd_acp3x_pcm_dma"
-  #   "snd_acp3x_i2s"
-  #   "snd_pci_acp3x"
-  #   "snd_soc_acp3x_es8336_mach"
-  # ];
-  # boot.blacklistedKernelModules = [
-  #   "snd_acp3x_rn"
-  #   "snd_pci_acp5x"
-  #   "snd_rn_pci_acp3x"
-  # ];
 }
