1fac929d4f94a44b190b24c7fd736c6fa6cc8523
Egor Platohin <e.platohin@eteron.ru>
2025-04-22 15:31:30 +0300

    Set env var

--- machines/huawei-mate15/env.nix
+++ machines/huawei-mate15/env.nix
@@ -2,7 +2,9 @@
   environment.variables = {
     EDITOR = "nano";
     # RANGER_LOAD_DEFAULT_RC = "FALSE";
-    # QT_QPA_PLATFORMTHEME = "qt5ct";
     # GSETTINGS_BACKEND = "keyfile";
+  };
+  environment.sessionVariables = {
+    QT_QPA_PLATFORM = "wayland";
   };
 }
