198c023e083080b7ef99cf006fab31ad4fbb474b
Egor Platohin <e.platohin@eteron.ru>
2025-04-22 21:00:53 +0300

    Fix

--- machines/htpc/gpu.nix
+++ machines/htpc/gpu.nix
@@ -65,7 +65,7 @@
     "nouveau.modeset=1" # Включить modesetting
   ];
   environment.systemPackages = with pkgs; [
-    nouveau-dmi
+    # nouveau-dmi
     mesa-demos # Для тестов (glxinfo, glxgears)
   ];
 }
