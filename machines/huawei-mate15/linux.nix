f1fbb4d067bc47784dbe1b7d2b1dea633d1eab55
Egor Platohin <e.platohin@eteron.ru>
2025-04-22 18:15:30 +0300

    Tune kernel modules

--- machines/huawei-mate15/linux.nix
+++ machines/huawei-mate15/linux.nix
@@ -1,9 +1,4 @@
 { pkgs, ... }:
 {
   boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_13;
-  boot.kernelModules = [
-    "overlay"
-    "fuse"
-  ];
-  boot.blacklistedKernelModules = [ "aufs" ];
 }
