e1042ab5bc9c4113059b3bdbc1eb0a77e217f277
Egor Platohin <e.platohin@eteron.ru>
2025-06-11 12:09:51 +0300

    Use latest kernel

--- flake.nix
+++ flake.nix
@@ -60,7 +60,6 @@
             nixos-hardware.nixosModules.common-pc-laptop
             nixos-hardware.nixosModules.common-pc-laptop-ssd
             nixos-hardware.nixosModules.common-cpu-amd
-            nixos-hardware.nixosModules.common-cpu-amd-zenpower
             nixos-hardware.nixosModules.common-gpu-amd
           ];
         };
--- machines/huawei-mate15/linux.nix
+++ machines/huawei-mate15/linux.nix
@@ -1,10 +1,26 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_13;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  boot.kernelModules = [
+    "amd_pstate"
+    "acpi_cpufreq"
+    "powernow-k8"
+  ];
   boot.extraModprobeConfig = ''
     options fuse allow_other
   '';
   boot.kernelParams = [
+    "amd_pstate=active"
     "i8042.nopnp"
+  ];
+
+  powerManagement.cpuFreqGovernor = "schedutil";
+  services.power-profiles-daemon.enable = true;
+  hardware.cpu.amd.updateMicrocode = true;
+
+  system.requiredKernelConfig = with config.lib.kernelConfig; [
+    (isYes "X86_AMD_PSTATE")
+    (isYes "X86_FEATURE_CPPC")
   ];
 }
