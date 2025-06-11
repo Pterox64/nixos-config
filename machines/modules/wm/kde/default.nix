0309d58ad0ff3e8d924251e223291c1f0d58abe5
Egor Platohin <e.platohin@eteron.ru>
2025-06-11 11:22:07 +0300

    Remove kconsole

--- machines/modules/wm/kde/default.nix
+++ machines/modules/wm/kde/default.nix
@@ -8,5 +8,8 @@
   services.displayManager.sddm.wayland.enable = true;
 
   services.desktopManager.plasma6.enable = true;
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    konsole # терминал KDE
+  ];
   security.pam.services.sddm.kwallet.enable = true;
 }
