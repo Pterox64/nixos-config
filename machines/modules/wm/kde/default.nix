a91fe4bf239d71e8b14180cf55ecd129d05c064c
Egor Platohin <e.platohin@eteron.ru>
2025-07-03 12:02:09 +0300

    fix

--- machines/modules/wm/kde/default.nix
+++ machines/modules/wm/kde/default.nix
@@ -3,9 +3,11 @@
   # Disable the X11 windowing system.
   services.xserver.enable = false;
   # Display Manager (SDDM) и KDE Plasma 6 в Wayland
-  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
   services.displayManager.defaultSession = "plasma";
-  services.displayManager.sddm.wayland.enable = true;
 
   services.desktopManager.plasma6.enable = true;
   # environment.plasma6.excludePackages = with pkgs.kdePackages; [
