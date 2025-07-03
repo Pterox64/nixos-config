5017e6e90d3dba1a6bda236032c4d26749ec6979
Egor Platohin <e.platohin@eteron.ru>
2025-07-03 12:03:03 +0300

    Add locale var

--- machines/modules/i18n/ru_RU.nix
+++ machines/modules/i18n/ru_RU.nix
@@ -1,20 +1,29 @@
-{ pkgs, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   i18n.defaultLocale = "ru_RU.UTF-8";
   i18n.supportedLocales = [
     "ru_RU.UTF-8/UTF-8"
-    "C.UTF-8/UTF-8"
-    "en_US.UTF-8/UTF-8"
   ];
-  # services.xserver.xkb = {
-  # layout = "us,ru";
-  # options = "grp:win_space_toggle";
-  # };
+  environment.variables = {
+    "LC_CTYPE" = "ru_RU.UTF-8";
+    "LC_ALL" = "ru_RU.UTF-8";
+  };
+
   console = {
     earlySetup = true;
     packages = [ pkgs.terminus_font ];
     font = "cyr-sun16";
     keyMap = lib.mkDefault "ru";
-    # useXkbConfig = true; # use xkb.options in tty.
+  };
+
+  systemd.services.display-manager.environment = lib.mkIf (config.services.displayManager.enable) {
+    "LANG" = "ru_RU.UTF-8";
+    "LC_CTYPE" = "ru_RU.UTF-8";
+    "LC_ALL" = "ru_RU.UTF-8";
   };
 }
